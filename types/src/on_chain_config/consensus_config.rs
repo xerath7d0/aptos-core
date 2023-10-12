8412e01ea5be891d68d21b86362d4921c31bd0ca
Zekun Li <li.zekun@gmail.com>
2023-10-12 12:31:01 -0500

    [TEST]

--- types/src/on_chain_config/consensus_config.rs
+++ types/src/on_chain_config/consensus_config.rs
@@ -77,7 +77,8 @@ impl OnChainConsensusConfig {
 /// This is used when on-chain config is not initialized.
 impl Default for OnChainConsensusConfig {
     fn default() -> Self {
-        OnChainConsensusConfig::V2(ConsensusConfigV1::default())
+        OnChainConsensusConfig::DagV1(DagConsensusConfigV1::default())
+        // OnChainConsensusConfig::V2(ConsensusConfigV1::default())
     }
 }
 
