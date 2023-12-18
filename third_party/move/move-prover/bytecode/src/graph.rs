a8d1eef702a306820c72cc8ca7173669148f67db
xerath <xerath7d0@gmail.com>
2023-12-18 08:54:53 -0500

    make fields of `Graph` public

--- third_party/move/move-prover/bytecode/src/graph.rs
+++ third_party/move/move-prover/bytecode/src/graph.rs
@@ -20,11 +20,11 @@ pub struct NaturalLoop<T: Ord + Copy + Debug> {
 }
 
 pub struct Graph<T: Ord + Copy + Debug> {
-    entry: T,
-    nodes: Vec<T>,
-    edges: Vec<(T, T)>,
-    predecessors: BTreeMap<T, BTreeSet<T>>,
-    successors: BTreeMap<T, BTreeSet<T>>,
+    pub entry: T,
+    pub nodes: Vec<T>,
+    pub edges: Vec<(T, T)>,
+    pub predecessors: BTreeMap<T, BTreeSet<T>>,
+    pub successors: BTreeMap<T, BTreeSet<T>>,
 }
 
 impl<T: Ord + Copy + Debug> Graph<T> {
